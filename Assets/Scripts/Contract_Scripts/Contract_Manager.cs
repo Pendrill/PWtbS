471db77761a9d9d7fd371bd9629248b3402a2e9a
Aleksei Waddington <Aleksei Waddington>
2017-02-20 16:30:49 -0500

    Reorganised some files. Started work on contract selection

---
+++ Assets/Scripts/Contract_Scripts/Contract_Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contract_Manager : MonoBehaviour {
+
+	//public Vector3 moveContract, focusContract, returnContract;
+	//public float time;
+	//public Contract_Selection theContractSelection;
+	// Use this for initialization
+	public SpriteRenderer panelColor;
+	float time;
+	Color currentAlpha, destinationAlpha;
+	public bool contractGotSelected;
+	public string ContractTitle, Objective, Target;
+	public Text ContractTXT, ObjectiveTXT, TargetTXT;
+	public Button button;
+	public Vector3 ContractOP, ObjectiveOP, TargetOP, ContractD, ObjectiveD, TargetD, ButtonOP, ButtonD;
+	void Start () {
+		//theContractSelection = FindObjectOfType<Contract_Selection> ();
+		currentAlpha = panelColor.color;
+		destinationAlpha = new Color (1, 1, 1, 0.63f);
+		ContractOP = ContractTXT.rectTransform.anchoredPosition3D;
+		ObjectiveOP = ObjectiveTXT.rectTransform.anchoredPosition3D;
+		TargetOP = TargetTXT.rectTransform.anchoredPosition3D;
+		ButtonOP = button.GetComponent<RectTransform> ().anchoredPosition3D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (contractGotSelected) {
+			time += Time.deltaTime * 2;
+			panelColor.color = Color.Lerp (currentAlpha, destinationAlpha, time);
+			ContractTXT.text = "Contract: " + ContractTitle;
+			ObjectiveTXT.text = "Objective: " + Objective;
+			TargetTXT.text = "Target: " + Target;
+			ContractTXT.rectTransform.anchoredPosition3D = Vector3.Lerp (ContractOP, ContractD, time);
+			ObjectiveTXT.rectTransform.anchoredPosition3D = Vector3.Lerp (ObjectiveOP, ObjectiveD, time);
+			TargetTXT.rectTransform.anchoredPosition3D = Vector3.Lerp (TargetOP, TargetD, time);
+			button.GetComponent<RectTransform> ().anchoredPosition3D = Vector3.Lerp (ButtonOP, ButtonD, time);
+
+		}
+	}
+
+	public void setUpSelectedContract (GameObject contract){
+		ContractTitle = contract.GetComponent<Contract_Selection> ().ContractTitle;
+		Objective = contract.GetComponent<Contract_Selection> ().Objective;
+		Target = contract.GetComponent<Contract_Selection> ().Target;
+
+	}
+	public void moveUnselectedContractsAwayFromBar(GameObject contract){
+
+	}
+}
---
+++ Assets/Scripts/Contract_Scripts/Contract_Selection.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contract_Selection : MonoBehaviour {
+
+	public TranslatorManager theTranslatorManager;
+	public Contract_Manager theContractManager;
+	public bool contractGotSelected, aSelectionHappened;
+	public Vector3 contractLocationOnBar, movedContract, selectedContract;
+	public float time;
+	public GameObject outline;
+
+	public string ContractTitle, Objective, Target;
+
+	// Use this for initialization
+	void Start () {
+		//contractGotSelected = true;
+		//theTranslatorManager = FindObjectOfType<TranslatorManager> ();
+		theContractManager = FindObjectOfType<Contract_Manager> ();
+		contractLocationOnBar = gameObject.transform.position;
+		movedContract = new Vector3 (13, 1, -0.64f);
+		selectedContract = new Vector3 (-2, 1, -6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hoverOverContract ();
+		if (Input.GetKeyDown (KeyCode.Mouse0) ){//&& !theContractManager.contractGotSelected ){//&& !theTranslatorManager.panelIsActive) {
+			//we create a ray cast that is emmited forward from the position of the mouse
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			//if there is a hit
+			if (Physics.Raycast (ray, out hit)) {
+				//Debug.Log ("hit");
+				aSelectionHappened = true;
+				//we only want to run this for the specific object that got hit (as this script will be attached to many objects)
+				if (hit.collider.gameObject.name == this.gameObject.name) {
+					theContractManager.contractGotSelected = true;
+					theContractManager.setUpSelectedContract (hit.collider.gameObject);
+					contractGotSelected = true;
+					//aSelectionHappened = true;
+				} else {
+					contractGotSelected = false;
+
+				}
+			}
+
+		}
+		if (!contractGotSelected && aSelectionHappened) {
+			time += Time.deltaTime * 2;
+			transform.position = Vector3.Lerp (contractLocationOnBar, movedContract, time);
+			//theContractManager.moveUnselectedContractsAwayFromBar (this.gameObject);
+		} else if (contractGotSelected && aSelectionHappened) {
+			time += Time.deltaTime * 2;
+			transform.position = Vector3.Lerp (contractLocationOnBar, selectedContract, time);
+		}
+	}
+
+	public void hoverOverContract(){
+		if (contractGotSelected) {
+			outline.SetActive (false);
+			return;
+		}
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			//we only want to run this for the specific object that got hit (as this script will be attached to many objects)
+			if (hit.collider.gameObject.name == this.gameObject.name) {
+				outline.SetActive (true);
+			} else {
+				outline.SetActive (false);
+			}
+		} else {
+			outline.SetActive (false);
+		}
+	}
+}
