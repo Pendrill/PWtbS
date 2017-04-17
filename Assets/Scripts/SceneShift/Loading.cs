3a51470d3191b9bcbb9586405a8fbe48f2596d22
Aleksei Waddington <amw694@nyu.edu>
2017-04-16 20:12:16 -0400

    Loading screen stuff

---
+++ Assets/Scripts/SceneShift/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Load());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public IEnumerator Load()
+    {
+        yield return new WaitForSeconds(2.9f);
+        gameObject.SetActive(false);
+
+    }
+}
