c9f2a19715ef6bd1101299a58638ec2ca7eff1be
Aleksei Waddington <amw694@nyu.edu>
2017-05-12 11:05:03 -0400

    New system of finding key words

---
+++ Assets/Scenes/PrototypeScenes/DisplaySingleWord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class DisplaySingleWord : MonoBehaviour {
+
+    //need an array of scrambled words
+    //need an array of 
+    public string thought;
+    public string[] keyWords;
+    //public static List<string> keyWords = new List<string>();
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log(System.Array.FindIndex(keyWords, findWord));
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public bool findWord(string word)
+    {
+        if (word.Trim().Equals(thought.Trim()))
+        {
+            return true;
+        }
+        return false;
+    }
+}
