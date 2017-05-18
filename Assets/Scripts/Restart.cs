7ad54dc8e382748d8337c64bec82aba6191785c0
Aleksei Waddington <amw694@nyu.edu>
2017-05-18 17:22:14 -0400

    debugging i guess

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+    public GameObject notebook;
+    public GameObject charger;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.P)){
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                notebook.GetComponent<ObjectInfo>().currentScene = "Classroom";
+                notebook.GetComponent<ObjectInfo>().inInv = false;
+                charger.GetComponent<ObjectInfo>().currentScene = "Classroom";
+                charger.GetComponent<ObjectInfo>().inInv = false;
+                QuestManager.currentContract = 0;
+                SceneManager.LoadScene("TitleScreen");
+
+            }
+
+        }
+    }
+}
